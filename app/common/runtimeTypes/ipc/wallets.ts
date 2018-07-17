1f79a1ac09522c47274affbc0e69a0cc940f5628
Johann Og <kronolynx@gmail.com>
2018-07-17 13:10:11 +0200

    feat(rt-types): add EncryptWalletParams

--- app/common/runtimeTypes/ipc/wallets.ts
+++ app/common/runtimeTypes/ipc/wallets.ts
@@ -1,5 +1,5 @@
-import { Empty } from "app/common/runtimeTypes/index"
-import { Wallets } from "app/common/runtimeTypes/storage/wallets"
+import {Empty} from "app/common/runtimeTypes/index"
+import {Wallets} from "app/common/runtimeTypes/storage/wallets"
 import * as t from "io-ts"
 
 export const GetWalletsParams = Empty
@@ -7,3 +7,15 @@ export type GetWalletsParams = t.TypeOf<typeof GetWalletsParams>
 
 export const GetWalletsResponse = Wallets
 export type GetWalletsResponse = t.TypeOf<typeof GetWalletsResponse>
+
+export const EncryptWalletParams = t.intersection([
+  t.type({
+    id: t.string,
+    password: t.string
+  }),
+  t.partial({
+    caption: t.string
+  })
+])
+
+export type EncryptWalletParams = t.TypeOf<typeof EncryptWalletParams>
